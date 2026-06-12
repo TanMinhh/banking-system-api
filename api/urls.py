1a2b2b399cb4523c01c237618e872c44ad3fa1ab
Minh Lê <22520865@gm.uit.edu.vn>
2026-06-12 22:16:48 +0700

    init api

---
+++ api/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+
+from .views import (
+    BranchesAPIView,
+    BranchDetailAPIView,
+    BanksAPIView,
+    BankDetailAPIView,
+    CreateAccountAPIView,
+    AccountListAPIView
+)
+
+urlpatterns = [
+    path('branches/', BranchesAPIView.as_view(),name='branches'),
+    path('branch/(?P<pk>[0-9]+)/', BranchDetailAPIView.as_view(),name='branch-detail'),
+    path('banks', BanksAPIView.as_view(), name='banks'),
+    path('bank/(?P<pk>[0-9]+)/', BankDetailAPIView.as_view(), name='bank-detail'),
+    path('create_account/', CreateAccountAPIView.as_view(), name='create-account'),
+    path('ccounts/', AccountListAPIView.as_view(), name='accounts'),
+]
